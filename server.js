81e2edefab94ec67282b80fe5a8e1c7cac7d05af
Mhjba <mahjoubaaitalla15@gmail.com>
2024-09-03 14:48:25 +0200

    files manager

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import router from './routes/index';
 
-const PORT = +(process.env.PORT) || 5000;
+const port = parseInt(process.env.PORT, 10) || 5000;
+
 const app = express();
 
 app.use(express.json());
 app.use('/', router);
-app.listen(PORT, () => {
-  console.log(`App is listening on Port ${PORT}`);
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
 
-module.exports = app;
+export default app;
