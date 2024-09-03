2652114008b1e2b9fff23250bb59ab8c71fde2c0
Mhjba <mahjoubaaitalla15@gmail.com>
2024-09-03 11:07:06 +0200

    files manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,23 @@
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-export default class AppController {
+class AppController {
   static getStatus(req, res) {
-    res.status(200).json({
+    const status = {
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    });
+    };
+    console.log(status);
+    res.status(200).json(status);
   }
 
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).json(stats);
   }
 }
+
+module.exports = AppController;
