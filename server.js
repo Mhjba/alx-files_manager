fd8f98e6edc36b3bf22353c36a4f82b9dbaf3806
Mhjba <mahjoubaaitalla15@gmail.com>
2024-09-03 11:23:25 +0200

    files manager

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import startServer from './libs/boot';
-import injectRoutes from './routes';
-import injectMiddlewares from './libs/middlewares';
+const express = require('express');
+const routes = require('./routes');
 
-const server = express();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-injectMiddlewares(server);
-injectRoutes(server);
-startServer(server);
+app.use(routes);
 
-export default server;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
