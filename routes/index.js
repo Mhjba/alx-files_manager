a1dd2b1cdcd4d483997fed29ff2938f931d3eb0e
Mhjba <mahjoubaaitalla15@gmail.com>
2024-09-04 11:06:40 +0200

    files manager

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import UsersController from '../controllers/UsersController.js';
 import AuthController from '../controllers/AuthController.js';
 import FilesController from '../controllers/FilesController.js';
 
+
 const router = Router();
 
 router.get('/status', AppController.getStatus);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/index.js'; // تأكد من إضافة .js إلى نهاية المسار
 
+
 const port = parseInt(process.env.PORT, 10) || 5000;
 
 const server = express();
