4004a95c5d49c9ac86046c177416334c87484213
Mhjba <mahjoubaaitalla15@gmail.com>
2024-09-04 15:01:42 +0200

    files manager

--- utils/db.js
+++ utils/db.js
@@ -3,26 +3,57 @@ import { MongoClient } from 'mongodb';
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
+    const db = process.env.DB_DATABASE || 'files_manager';
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-
-    this.client = new MongoClient(`mongodb://${host}:${port}`, { useUnifiedTopology: true });
-    this.client.connect().then(() => {
-      this.db = this.client.db(database);
-      this.usersCollection = this.db.collection('users');
+    const url = `mongodb://${host}:${port}`;
+    MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      if (err) {
+        console.log(err.message);
+        this.db = false;
+      } else {
+        this.db = client.db(db);
+      }
     });
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return !!this.db;
   }
 
   async nbUsers() {
-    return this.usersCollection.countDocuments();
+    return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.filesCollection.countDocuments();
+    return this.db.collection('files').countDocuments();
+  }
+
+  async findUser(user) {
+    return this.db.collection('users').findOne(user);
+  }
+
+  async createUser(user) {
+    return this.db.collection('users').insertOne(user);
+  }
+
+  async updateUser(query, update) {
+    return this.db.collection('users').updateOne(query, update);
+  }
+
+  async findFile(file) {
+    return this.db.collection('files').findOne(file);
+  }
+
+  async createFile(file) {
+    return this.db.collection('files').insertOne(file);
+  }
+
+  async aggregateFiles(query) {
+    return this.db.collection('files').aggregate(query).toArray();
+  }
+
+  async updateFile(query, update) {
+    return this.db.collection('files').updateOne(query, update);
   }
 }
 
