dc8cc89110ee8792bebf8a0d18cc3dfeaa1dd578
Mhjba <mahjoubaaitalla15@gmail.com>
2024-10-14 20:45:43 +0200

    files manager

--- utils/redis.js
+++ utils/redis.js
@@ -2,31 +2,53 @@ const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-  constructor() {
-    this.client = redis.createClient();
+    constructor() {
+        this.client = createClient();
 
-    this.getAsync = promisify(this.client.get).bind(this.client);
+        this.client.on('error', (err) => {
+            console.error('Redis client not connected to the server:', err);
+        });
+    }
 
-    this.client.on('error', (error) => {
-      console.log(`An error occurred trying to connect to the server: ${error.message}`);
-    });
-  }
+    isAlive() {
+        return this.client.connected;
+    }
 
-  isAlive() {
-    return this.client.connected;
-  }
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, value) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(value);
+                }
+            });
+        });
+    }
 
-  async get(key) {
-    return this.getAsync(key);
-  }
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', duration, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
 
-  async set(key, value, duration) {
-    this.client.setex(key, duration, value);
-  }
-
-  async del(key) {
-    this.client.del(key);
-  }
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    }
 }
 
 const redisClient = new RedisClient();
