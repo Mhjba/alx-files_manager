1407be50ed42a2e33b6c9106fc3c2d1dc0f4c37f
Mhjba <mahjoubaaitalla15@gmail.com>
2024-10-15 21:55:30 +0200

    files manager

--- utils/db.js
+++ utils/db.js
@@ -1,12 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-// Set database connection details.
 const HOST = process.env.DB_HOST || 'localhost';
 const PORT = process.env.DB_PORT || 27017;
 const DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${HOST}:${PORT}`;
 
-// Define the DBClient class to manage MongoDB operations.
 class DBClient {
   constructor() {
     this.client = new MongoClient(url, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -17,19 +15,16 @@ class DBClient {
     });
   }
 
-  //check if the database client is connected.
   isAlive() {
     return this.client.isConnected();
   }
 
-  // get the count of documents in the 'users' collection.
   async nbUsers() {
     const users = this.db.collection('users');
     const usersNum = await users.countDocuments();
     return usersNum;
   }
 
-  // get the count of documents in the 'files' collection.
   async nbFiles() {
     const files = this.db.collection('files');
     const filesNum = await files.countDocuments();
@@ -37,7 +32,5 @@ class DBClient {
   }
 }
 
-// Create an instance of the DBClient.
 const dbClient = new DBClient();
 module.exports = dbClient;
-
