ad7c25f795d03d9b71bef5a81776c0a67cba97f9
Mhjba <mahjoubaaitalla15@gmail.com>
2024-10-15 19:36:05 +0200

    files manager

--- utils/redis.js
+++ utils/redis.js
@@ -1,5 +1,7 @@
 import redis from 'redis';
+import { promisify } from 'util';
 
+// class to define methods for commonly used redis commands
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
@@ -13,6 +15,7 @@ class RedisClient {
     return this.client.connected;
   }
 
+// get value for given key from redis server
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (error, reply) => {
@@ -25,6 +28,7 @@ class RedisClient {
     });
   }
 
+// set key value pair to redis server
   async set(key, value, durationInSeconds) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, durationInSeconds, value, (error, reply) => {
@@ -37,6 +41,7 @@ class RedisClient {
     });
   }
 
+// del key vale pair from redis server
   async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (error, reply) => {
